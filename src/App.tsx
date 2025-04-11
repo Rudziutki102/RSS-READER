a39332e00bd871bd229cdea09d4e1a0ac0e8f8fc
Mateusz <mateusz.makowski.102@gmail.com>
2025-04-11 15:21:45 +0200

    Batman

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <div className='flex justify-center items-center'>eldorado koleszkowie</div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { persistQueryClient, removeOldestQuery } from '@tanstack/react-query-persist-client'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import './index.css'
+import App from './App.tsx'
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24,
+    }
+  }
+})
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+  retry: removeOldestQuery,
+  key: 'rss-reader-cache',
+})
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+})
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
